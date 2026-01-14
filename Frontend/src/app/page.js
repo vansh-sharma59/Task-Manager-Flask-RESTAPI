5dba77a634729629ebaac72db48082ba0a75ccd7
Vansh Sharma <96278749+vansh-sharma59@users.noreply.github.com>
2026-01-14 17:18:03 +0530

    Update page.js to redirect to register page

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      There is no home page.
-    </div>
-  );
+  redirect("/register");
 }
